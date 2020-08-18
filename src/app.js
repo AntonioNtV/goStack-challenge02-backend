cdd4773c1494089d2ceead78a14c9c9a9596c70a
AntonioNtV <antoniontv18@gmail.com>
2020-08-18 08:30:02 -0300

    Funcionalidade de criar um repository, atualizar um repository, deletar um repository

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { v4, validate } = require("uuid");
+const { v4 } = require("uuid");
 
 const app = express();
 
@@ -10,19 +10,67 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+    return response.status(200).json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+
+  const repository = { 
+    id: v4(), 
+    title,
+    url,
+    techs, 
+    likes: 0 
+  }
+
+  repositories.push(repository)
+
+  return response.status(200).json(repository)
+
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+  const { url, title, techs } = request.body
+
+  const repositoryIndex = repositories.findIndex( repository => repository.id === id)
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({
+      error: 'Repository does not exist'
+    })
+  }
+
+  const repository = {
+    id,
+    url,
+    title,
+    techs
+  }
+
+  repositories[repositoryIndex] = repository
+
+  return response.status(200).json(repository)
+
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params
+
+  const repositoryIndex = repositories.findIndex( repository => repository.id === id)
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({
+      error: 'Repository does not exist'
+    })
+  }
+
+  repositories.splice(repositoryIndex, 1)
+
+
+  return response.status(204).send()
+
 });
 
 app.post("/repositories/:id/like", (request, response) => {
